2f2af0b2b9d2de3884e80ea4e78c62397dfaba86
ADMIN <ADMIN@user>
2017-10-08 11:19:17 -0500

    Probando proyecto en maven

---
+++ miPrimerMaven/src/main/java/com/mycompany/ComPruebaEmail.java
@@ -0,0 +1,25 @@
+package com.mycompany;
+
+import java.awt.EventQueue;
+
+import javax.swing.JOptionPane;
+
+public class ComPruebaEmail {
+
+	public static void main(String[] args) {
+		boolean arroba = false;
+		String mail = JOptionPane.showInputDialog("Intorduce Email");
+		
+		for (int i=0; i < mail.length(); i++){
+			if(mail.charAt(i) == '@' ){
+			arroba = true;	
+			}
+		}
+		if(arroba == true){
+			System.out.println("Es Correcto");	
+		}else {
+			System.out.println("Es InCorrecto");
+		}
+		
+	}	
+}
